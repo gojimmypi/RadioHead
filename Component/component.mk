ac8827d9a94b32a3eda6f544af7ba66ee32a4901
gojimmypi <gojimmypi@gmail.com>
2018-06-11 14:43:10 -0700

    project file auto-update

---
+++ Component/component.mk
@@ -0,0 +1,4 @@
+#Created by VisualGDB. Right-click on the component in Solution Explorer to edit properties using convenient GUI.
+
+
+COMPONENT_SRCDIRS += ../../../../../../workspace-git/EmbeddedInclude/EmbeddedInclude/components/RadioHead ..
